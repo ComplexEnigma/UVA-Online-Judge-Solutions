2d702b059a1c55752b1e760274cdb7a94257072d
Symon Hasan <34129377+ComplexEnigma@users.noreply.github.com>
2018-03-10 14:49:39 +0600

    Create UVA_12279.cpp

---
+++ UVA_12279.cpp
@@ -0,0 +1,59 @@
+/***
+         _______________________________
+
+             MD. SYMON HASAN SHOHAN
+             UNIVERSITY OF ASIA PACIFIC
+             isymonhs@gmail.com
+         _______________________________
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define filein() freopen("input.txt","r",stdin)
+#define file() {freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);}
+#define fastio() { ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define GraphString map<string, vector<string> > G
+#define GraphInt map<int,vector<int> > G
+#define VisitedString map<string,int> visited
+#define VisitedInt map<int,int> visited
+#define PrevString map<string,string> previous
+#define PrevInt map<int,int> previous
+
+#define Size(x) x.size()
+#define Front(x) x.front()
+#define Pop(x) x.pop()
+#define Push(x,y) x.push(y)
+#define Empty(x) x.empty()
+#define PresentInMap(x,y) x.count(y)
+#define PB(x,y) x.push_back(y)
+#define Clear(x) x.clear()
+#define FillVector(x) fill(x.begin(),x.end(),-1)
+#define CreateVectorInt(x) vector<int> x
+
+void solve()
+{
+    int n,a,c=1;
+    while(scanf("%d",&n)==1 && n!=0)
+    {
+        int counter1=0,counter2=0;
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&a);
+            if(a==0)
+                counter2++;
+            else
+                counter1++;
+        }
+        printf("Case %d: %d\n",c,counter1-counter2);
+        c++;
+    }
+}
+
+int main()
+{
+    //ios_base::sync_with_stdio(0);cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    solve();
+    return 0;
+}
