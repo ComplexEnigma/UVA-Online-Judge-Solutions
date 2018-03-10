6da695862e94da95445e2be9635db36c3e1e26e2
Symon Hasan <34129377+ComplexEnigma@users.noreply.github.com>
2018-03-10 14:44:16 +0600

    Create UVA_11172.cpp

---
+++ UVA_11172.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,a,b;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d %d",&a,&b);
+		if(a>b)
+		{
+			printf(">\n");
+		}
+		else if(b>a)
+		{
+			printf("<\n");
+		}
+		else if(a==b)
+		{
+			printf("=\n");
+		}
+	}
+}
