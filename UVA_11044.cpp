3d71c008bdabb7aab64144cabc427fb0d72809ef
Symon Hasan <34129377+ComplexEnigma@users.noreply.github.com>
2018-03-10 14:43:04 +0600

    Create UVA_11044.cpp

---
+++ UVA_11044.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*                  --------- Author ----------|
+                    | MD. SYMON HASAN SHOHAN    |
+                    | University Of Asia Pacific|
+                    | isymonhs@gmail.com        |
+                    |---------------------------|
+                                                                            */
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        int a=(n/3)*(m/3);
+        cout<<a<<endl;
+    }
+    return 0;
+}
