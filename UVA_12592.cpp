6a5e828afd0c2744e85c15b58c2294bcd0da58ca
Symon Hasan <34129377+ComplexEnigma@users.noreply.github.com>
2018-03-18 19:20:12 +0600

    Create UVA_12592.cpp

---
+++ UVA_12592.cpp
@@ -0,0 +1,76 @@
+// Author : ComplexEnigma
+// Date :
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<list>
+#include<stdlib.h>
+#include<string>
+#include<string.h>
+#include<utility>
+using namespace std;
+
+typedef long long ll;
+
+#define filein        {freopen("input.txt","r",stdin);}
+#define fileout     {freopen("output.txt","w",stdout);}
+#define fastio      {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+
+#define PB(x,y) x.push_back(y)
+#define Clear(x) x.clear()
+#define Resize(x) x.resize()
+#define Fill(x,y) fill(x.begin(),x.end(),y)
+#define Sort(x) sort(x.begin(),x.end())
+#define rep(i,n) for( i=0; i<n; i++)
+#define REP(i,k,n) for( i=k; i<n; i++)
+#define REPR(i,k,n) for( i=k; i>n; i--)
+
+int _I() {int x;scanf("%d",&x);return x;}
+ll _LL() {ll x;scanf("%lld",&x);return x;}
+float _F() {float x;scanf("%f",&x);return x;}
+double _LF() {double x;scanf("%lf",&x);return x;}
+
+map <string,string> m;
+
+void PreCall()
+{
+
+}
+
+void C_Enigma()
+{
+    //PreCall();
+    int N = _I();
+    getchar();
+    while( N-- )
+    {
+        string s1,s2;
+        getline(cin,s1);
+        getline(cin,s2);
+        m[s1] = s2;
+    }
+    int Q = _I();
+    getchar();
+    while( Q-- )
+    {
+        string s1;
+        getline(cin,s1);
+        cout << m[s1] << endl;
+    }
+}
+
+int main()
+{
+    //fastio;
+    //filein;fileout;
+    C_Enigma();
+
+    return 0;
+}
