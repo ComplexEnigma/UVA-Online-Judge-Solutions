ec2d28cdb83c0c24c166652d89d7cf786012d34f
Symon Hasan <34129377+ComplexEnigma@users.noreply.github.com>
2018-03-27 01:00:28 +0600

    Create UVA_10922.cpp

---
+++ UVA_10922.cpp
@@ -0,0 +1,99 @@
+// Author : ComplexEnigma
+// Date :
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<list>
+#include<stdlib.h>
+#include<string>
+#include<string.h>
+#include<utility>
+using namespace std;
+
+typedef long long ll;
+
+#define filein        {freopen("input.txt","r",stdin);}
+#define fileout     {freopen("output.txt","w",stdout);}
+#define fastio      {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+
+#define PB(x,y) x.push_back(y)
+#define Clear(x) x.clear()
+#define Resize(x) x.resize()
+#define Fill(x,y) fill(x.begin(),x.end(),y)
+#define Sort(x) sort(x.begin(),x.end())
+#define rep(i,n) for( i=0; i<n; i++)
+#define REP(i,k,n) for( i=k; i<n; i++)
+#define REPR(i,k,n) for( i=k; i>n; i--)
+
+int _I()
+{
+    int x;
+    scanf("%d",&x);
+    return x;
+}
+ll _LL()
+{
+    ll x;
+    scanf("%lld",&x);
+    return x;
+}
+float _F()
+{
+    float x;
+    scanf("%f",&x);
+    return x;
+}
+double _LF()
+{
+    double x;
+    scanf("%lf",&x);
+    return x;
+}
+
+char num[1005];
+
+void C_Enigma()
+{
+    while( gets(num) )
+    {
+        if( strcmp(num,"0") == 0)
+            break;
+        int sum = 0;
+        for(int i=0; num[i]!='\0'; i++)
+        {
+            sum += num[i]-'0';
+        }
+        int degree = 1;
+        while(sum>9)
+        {
+            degree ++;
+            int temp = sum;
+            sum = 0;
+            while(temp)
+            {
+                sum+=temp%10;
+                temp/=10;
+            }
+        }
+        if (sum == 9)
+            cout << num << " is a multiple of 9 and has 9-degree " << degree << ".\n";
+        else
+            cout << num << " is not a multiple of 9.\n";
+    }
+}
+
+int main()
+{
+    //fastio;
+    //filein;fileout;
+    C_Enigma();
+
+    return 0;
+}
